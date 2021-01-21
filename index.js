4c26e1f2a96cb25a10b95696e428dc4d83df1a30
NtkArt <arthur.carneirontk@gmail.com>
2021-01-21 20:48:14 +0000

    Create: Recursive Func.

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 function factorial (n) {
-  let i=0;
-  if(n <= 1){
-    return n;
-  }else{
-    i++;
-    factorial(n*(n-i))
-  }
+  if(n < 2) return 1;
+  return n * factorial(n-1)
 }
 
 
 
-console.log(factorial(3))
+console.log(factorial(5))
